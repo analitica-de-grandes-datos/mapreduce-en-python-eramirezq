1888deb07d2b0b1896f74b94487c63e937adf756
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-06-26 12:15:49 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    def agregar_ceros_tres_digitos(valor):
+        return f'{valor:03d}'
+    
+    for line in sys.stdin:
+        letter,date,val=line.split()
+        val=int(val)
+        val=agregar_ceros_tres_digitos(val)
+        sys.stdout.write('{}\t{}\t{}\n'.format(val,letter,date))
