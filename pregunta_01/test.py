2139627060a8ad90c9ceadeec192fccc365a0b26
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-06-26 12:15:03 -0500

    Update test.py

--- pregunta_01/test.py
+++ pregunta_01/test.py
@@ -10,8 +10,8 @@ lines = [line.strip().replace("\n", "") for line in result.split("\n")]
 expected = """credit_history	1
 critical	293
 delayed	88
-fully repaid	40
 fully repaid this bank	49
+fully repaid	40
 repaid	530
 """.split(
     "\n"
