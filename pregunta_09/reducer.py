45ee34b554323624e39b1f10347325a0b5083fa3
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-06-26 12:15:52 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    cont=0
+    for line in sys.stdin:
+        cont+=1
+        val,letter,date=line.split()
+        val=int(val)
+        sys.stdout.write('{}   {}   {}\n'.format(letter,date,val))
+        if cont==6:
+            break
