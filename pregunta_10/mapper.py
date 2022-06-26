76c94df1441a4a4d1a631cbcb31dd989e19c52e4
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-06-26 12:15:55 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    
+    
+    for line in sys.stdin:
+        key=line.split()[0]
+        key=int(key)
+        letters=line.split()[1]
+        for letter in letters.split(','):
+            sys.stdout.write('{}\t{:03d}\n'.format(letter,key))
+        
+        
